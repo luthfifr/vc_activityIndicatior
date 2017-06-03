13b71857c63f1d71ddb21ba1667ccb238b01c783
Luthfi Fathur Rahman <LuthfiFR@LFR-MacBook-Air.local>
2017-06-03 15:26:10 +0700

    Initial Commit

---
+++ viewController_activityIndicator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  viewController_activityIndicator
+//
+//  Created by Luthfi Fathur Rahman on 5/21/17.
+//  Copyright © 2017 Luthfi Fathur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var btn_activityIndicator: UIButton!
+    
+    var activityIndicator = UIActivityIndicatorView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btn_activityIndicator.layer.cornerRadius = 5
+        
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btn_activityIndicator(_ sender: Any) {
+        activityIndicator.startAnimating()
+        btn_activityIndicator.backgroundColor = UIColor.gray
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        let delay = DispatchTime.now()+5
+        DispatchQueue.main.asyncAfter(deadline: delay){
+            self.activityIndicator.stopAnimating()
+            UIApplication.shared.endIgnoringInteractionEvents()
+            self.btn_activityIndicator.backgroundColor = UIColor.blue
+        }
+    }
+
+}
+
